a24f6a90b621c815c6ab4da8a209abd3abeb8d44
Dan Debrunner <ddebrunner@users.noreply.github.com>
2024-09-11 15:46:51 -0400

    missing file

---
+++ packages/gqltest/_introspection.js
@@ -0,0 +1,19 @@
+// Copyright IBM Corp. 2020, 2024
+
+const graphql = require('graphql')
+const introspectionTests = [
+  {
+    label: 'standard-introspection',
+    query: graphql.getIntrospectionQuery(),
+  },
+  {
+    // taken from npm graphl 0.9.1 which is the version used by get-graphql-schema 2.1.0
+    label: "introspection-june2018",
+    query: "\n  query IntrospectionQuery {\n    __schema {\n      queryType { name }\n      mutationType { name }\n      subscriptionType { name }\n      types {\n        ...FullType\n      }\n      directives {\n        name\n        description\n        locations\n        args {\n          ...InputValue\n        }\n      }\n    }\n  }\n\n  fragment FullType on __Type {\n    kind\n    name\n    description\n    fields(includeDeprecated: true) {\n      name\n      description\n      args {\n        ...InputValue\n      }\n      type {\n        ...TypeRef\n      }\n      isDeprecated\n      deprecationReason\n    }\n    inputFields {\n      ...InputValue\n    }\n    interfaces {\n      ...TypeRef\n    }\n    enumValues(includeDeprecated: true) {\n      name\n      description\n      isDeprecated\n      deprecationReason\n    }\n    possibleTypes {\n      ...TypeRef\n    }\n  }\n\n  fragment InputValue on __InputValue {\n    name\n    description\n    type { ...TypeRef }\n    defaultValue\n  }\n\n  fragment TypeRef on __Type {\n    kind\n    name\n    ofType {\n      kind\n      name\n      ofType {\n        kind\n        name\n        ofType {\n          kind\n          name\n          ofType {\n            kind\n            name\n            ofType {\n              kind\n              name\n              ofType {\n                kind\n                name\n                ofType {\n                  kind\n                  name\n                }\n              }\n            }\n          }\n        }\n      }\n    }\n  }\n"
+  },
+  {
+    label: "simple-__type",
+    query: "{__type(name:\"Query\"){name fields{name type{name}}}}"
+  }
+]
+exports.introspectionTests = introspectionTests;
