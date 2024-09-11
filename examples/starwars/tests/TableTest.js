af4fe919d94795880d7fdb6868384c201296ce54
Dan Debrunner <ddebrunner@users.noreply.github.com>
2024-09-11 08:56:22 -0400

    test: example use of table of tests

---
+++ examples/starwars/tests/TableTest.js
@@ -0,0 +1,58 @@
+const gqltest = require("gqltest/packages/gqltest/gqltest.js");
+const stepzen = require("gqltest/packages/gqltest/stepzen.js");
+
+endpoint =
+  "https://stepzen-chester.us-east-a.ibm.stepzen.net/examples/starwars/graphql";
+
+describe("table-driven", function () {
+  this.timeout(5000);
+  this.slow(1000);
+
+  const tests = [
+    {
+      label: "query",
+      query: "query {human(id:1001) {name}}",
+      expected: {
+        human: {
+          name: "Darth Vader",
+        },
+      },
+    },
+    {
+      label: "variables",
+      query: "query ($id:ID!) {droid(id:$id) {name}}",
+      variables: { id: 2001 },
+      expected: {
+        droid: {
+          name: "R2-D2",
+        },
+      },
+    },
+    {
+      label: "operationName",
+      query: "query Episode($ep:Episode!) {hero(episode: $ep) {name}}",
+      variables: { ep: "EMPIRE" },
+      operationName: "Episode",
+      expected: {
+        hero: {
+          name: "Luke Skywalker",
+        },
+      },
+    },
+    {
+      // the request can be in its own object
+      label: "request",
+      request: {
+        query: "query Episode($ep:Episode!) {hero(episode: $ep) {name}}",
+        variables: { ep: "JEDI" },
+        operationName: "Episode",
+      },
+      expected: {
+        hero: {
+          name: "R2-D2",
+        },
+      },
+    },
+  ];
+  gqltest.runtests("starwars", endpoint, stepzen.public(), tests);
+});
